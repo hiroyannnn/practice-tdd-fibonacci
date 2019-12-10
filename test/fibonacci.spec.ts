5f7d15b77a44b24a4a9ccca1d607f3d1b69548c1
hiroyannnn <36023909+hiroyannnn@users.noreply.github.com>
2019-12-11 00:24:38 +0900

    テストの実装 重複削除

--- test/fibonacci.spec.ts
+++ test/fibonacci.spec.ts
@@ -1,8 +1,13 @@
 import { fib } from "../src/fibonacci";
 
 describe("FibonacciTest", () => {
-  it("fib", () => {
-    expect(fib(0)).toBe(0);
-    expect(fib(1)).toBe(1);
+  const cases: Array<Array<number>> = [
+    [0, 0],
+    [1, 1]
+  ];
+  cases.forEach(testCase => {
+    it(`fib${testCase}`, () => {
+      expect(fib(testCase[0])).toBe(testCase[1]);
+    });
   });
 });
