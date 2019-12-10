f87e9d376fe732a38f85794811eeae941705323d
hiroyannnn <36023909+hiroyannnn@users.noreply.github.com>
2019-12-11 00:25:34 +0900

    テストの実装 パターン追加

--- test/fibonacci.spec.ts
+++ test/fibonacci.spec.ts
@@ -3,7 +3,9 @@ import { fib } from "../src/fibonacci";
 describe("FibonacciTest", () => {
   const cases: Array<Array<number>> = [
     [0, 0],
-    [1, 1]
+    [1, 1],
+    [2, 1],
+    [3, 2]
   ];
   cases.forEach(testCase => {
     it(`fib${testCase}`, () => {
