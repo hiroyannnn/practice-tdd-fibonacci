adc58730378df8b6f0299c35640019235ece0539
hiroyannnn <36023909+hiroyannnn@users.noreply.github.com>
2019-12-11 00:13:26 +0900

    テストの実装 fib(1) = 1

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 テスト駆動開発本に沿って、TDD の練習をするリポジトリ  
 このリポジトリに関しては、フィボナッチ数列の TDD をしてみる。
 
+## テストのしかた
+
+```bash
+npm run test
+```
+
 ## initialize project
 
 ```bash
--- test/fibonacci.spec.ts
+++ test/fibonacci.spec.ts
@@ -3,5 +3,6 @@ import { fib } from "../src/fibonacci";
 describe("FibonacciTest", () => {
   it("fib", () => {
     expect(fib(0)).toBe(0);
+    expect(fib(1)).toBe(1);
   });
 });
